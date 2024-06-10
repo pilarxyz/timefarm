c9245b0c85fc96abc615455263c90b48a3e2db73
pilarxyz <pilar@student.uns.ac.id>
2024-06-10 22:54:03 +0700

    upload

---
+++ README.md
@@ -0,0 +1,78 @@
+# Time Farm Telegram Bot
+
+This bot automates interactions with the Time Farm Telegram bot. It handles tasks such as fetching farming info, submitting tasks, and claiming rewards for multiple accounts. Each account is processed with a 5-second delay between them, and the bot rechecks the accounts every hour.
+
+## Refferal Link
+
+Click [here](https://t.me/TimeFarmCryptoBot?start=WIOAo6rSXEZHbmDz) to join Time Farm and get 30000 TIME for free.
+
+## Features
+
+- Fetch farming info for each account
+- Submit tasks and claim rewards automatically
+- Delays between processing each account to prevent rate-limiting
+- Regular rechecking of all accounts
+
+## Requirements
+
+- Node.js v12.x or higher
+- NPM or Yarn
+
+## Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/yourusername/TimeFarmTelegramBot.git
+   cd TimeFarmTelegramBot
+   ```
+
+2. Install the dependencies:
+
+   ```bash
+   npm install
+   ```
+
+3. Create `token.json` files in the root directory.
+
+   - `token.json` should contain your tokens:
+
+     ```json
+     [
+       "Bearer your_first_token",
+       "Bearer your_second_token",
+       "Bearer your_third_token"
+     ]
+     ```
+
+## Usage
+
+1. Run the bot:
+
+   ```bash
+   node index.js
+   ```
+
+2. The bot will start processing each account, fetching farming info, submitting tasks, and claiming rewards. It will print logs to the console with details about each operation.
+
+## Example Output
+
+```plaintext
+[ 10:26:56 PM ] Memproses akun ke-1...
+[ 10:26:57 PM ] Balance: 730000.000
+[ 10:26:57 PM ] Active Farming Started At (WIB): Jun 10, 2024, 8:26 PM
+[ 10:26:57 PM ] Next Claim Time (WIB): Jun 11, 2024, 12:26 AM
+[ 10:26:57 PM ] Task Follow our CEO on X sudah dikerjakan
+[ 10:26:57 PM ] Task Follow Time Farm on X sudah dikerjakan
+[ 10:26:57 PM ] Task Subscribe to our CEO channel belum bisa claim
+[ 10:26:57 PM ] Task Follow Chrono.tech on X sudah dikerjakan
+[ 10:26:57 PM ] Task Follow LaborX on X sudah dikerjakan
+[ 10:26:57 PM ] Task Subscribe to TimeFarm channel belum bisa claim
+[ 10:26:57 PM ] Task Subscribe to TimeFarm chat belum bisa claim
+[ 10:26:57 PM ] Task Subscribe to LaborX channel sudah dikerjakan
+[ 10:26:57 PM ] Task Subscribe to Chrono.tech channel sudah dikerjakan
+[ 10:26:57 PM ] Task Subscribe to Chrono.tech on YouTube sudah dikerjakan
+[ 10:26:57 PM ] Akun ke-1 menunggu 1 jam dan 59 menit sebelum klaim...
+[ 10:27:02 PM ] Memproses akun ke-2...
+...
+```
---
+++ index.js
@@ -0,0 +1,239 @@
+import axios from "axios";
+import fs from "fs";
+import chalk from "chalk";
+import { DateTime } from "luxon";
+
+const tokens = JSON.parse(fs.readFileSync("token.json", "utf-8"));
+const userAgents = JSON.parse(fs.readFileSync("useragents.json", "utf-8"));
+const convertedTokensPath = "token-converted.json";
+
+if (!fs.existsSync(convertedTokensPath)) {
+  fs.writeFileSync(convertedTokensPath, JSON.stringify([]));
+}
+
+const convertedTokens = JSON.parse(
+  fs.readFileSync(convertedTokensPath, "utf-8")
+);
+
+const getRandomUserAgent = () => {
+  return userAgents[Math.floor(Math.random() * userAgents.length)];
+};
+
+const getTimestamp = () => {
+  return `[ ${new Date().toLocaleTimeString()} ]`;
+};
+
+const getHeaders = (token) => ({
+  accept: "/",
+  "accept-language": "en-GB,en;q=0.8",
+  authorization: token,
+  "content-type": "application/json",
+  origin: "https://tg-tap-miniapp.laborx.io",
+  priority: "u=1, i",
+  referer: "https://tg-tap-miniapp.laborx.io/",
+  "sec-ch-ua": '"Brave";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
+  "sec-ch-ua-mobile": "?0",
+  "sec-ch-ua-platform": '"Windows"',
+  "sec-fetch-dest": "empty",
+  "sec-fetch-mode": "cors",
+  "sec-fetch-site": "same-site",
+  "sec-gpc": "1",
+  "user-agent": getRandomUserAgent(),
+});
+
+const getFarmingInfo = async (token) => {
+  try {
+    const response = await axios.get(
+      "https://tg-bot-tap.laborx.io/api/v1/farming/info",
+      {
+        headers: getHeaders(token),
+      }
+    );
+    const { balance, activeFarmingStartedAt, farmingDurationInSec } =
+      response.data;
+    const activeFarmingStartedWIB = DateTime.fromISO(activeFarmingStartedAt)
+      .setZone("Asia/Jakarta")
+      .toLocaleString(DateTime.DATETIME_MED);
+    const nextClaimTime = DateTime.fromISO(activeFarmingStartedAt)
+      .plus({ seconds: farmingDurationInSec + 10 })
+      .setZone("Asia/Jakarta")
+      .toLocaleString(DateTime.DATETIME_MED);
+    console.log(chalk.green(`${getTimestamp()} Balance: ${balance}`));
+    console.log(
+      chalk.green(
+        `${getTimestamp()} Active Farming Started At (WIB): ${activeFarmingStartedWIB}`
+      )
+    );
+    console.log(
+      chalk.green(`${getTimestamp()} Next Claim Time (WIB): ${nextClaimTime}`)
+    );
+    return { activeFarmingStartedAt, farmingDurationInSec };
+  } catch (error) {
+    console.log(
+      chalk.red(
+        `${getTimestamp()} Error saat mendapatkan farming info:`,
+        error.message
+      )
+    );
+    return null;
+  }
+};
+
+const claimReward = async (token, index) => {
+  const config = {
+    method: "post",
+    url: "https://tg-bot-tap.laborx.io/api/v1/farming/start",
+    headers: getHeaders(token),
+    data: {},
+  };
+
+  try {
+    const response = await axios(config);
+    if (response.status === 200) {
+      console.log(
+        chalk.green(`${getTimestamp()} Akun ke-${index + 1} Berhasil claim`)
+      );
+    }
+  } catch (error) {
+    if (error.response && error.response.status === 403) {
+      console.log(
+        chalk.yellow(`${getTimestamp()} Akun ke-${index + 1} Belum waktu claim`)
+      );
+    } else {
+      console.log(
+        chalk.red(
+          `${getTimestamp()} Akun ke-${index + 1} Error:`,
+          error.message
+        )
+      );
+    }
+  }
+  console.log(chalk.blue("------------------------------"));
+};
+
+const processTasks = async (token, index) => {
+  try {
+    const response = await axios.get(
+      "https://tg-bot-tap.laborx.io/api/v1/tasks",
+      {
+        headers: getHeaders(token),
+      }
+    );
+    const tasks = response.data;
+    let allClaimed = true;
+
+    for (const task of tasks) {
+      if (!task.submission) {
+        await axios.post(
+          `https://tg-bot-tap.laborx.io/api/v1/tasks/${task.id}/submissions`,
+          {},
+          {
+            headers: getHeaders(token),
+          }
+        );
+        console.log(
+          chalk.green(`${getTimestamp()} Berhasil submit task ${task.title}`)
+        );
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+      } else if (task.submission.status === "SUBMITTED") {
+        console.log(
+          chalk.yellow(`${getTimestamp()} Task ${task.title} belum bisa claim`)
+        );
+      } else if (task.submission.status === "COMPLETED") {
+        await axios.post(
+          `https://tg-bot-tap.laborx.io/api/v1/tasks/${task.id}/claims`,
+          {},
+          {
+            headers: getHeaders(token),
+          }
+        );
+        console.log(
+          chalk.green(`${getTimestamp()} Berhasil claim task ${task.title}`)
+        );
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+      } else if (task.submission.status === "CLAIMED") {
+        console.log(
+          chalk.blue(`${getTimestamp()} Task ${task.title} sudah dikerjakan`)
+        );
+      }
+
+      if (task.submission && task.submission.status !== "CLAIMED") {
+        allClaimed = false;
+      }
+    }
+
+    if (allClaimed) {
+      console.log(
+        chalk.green(`${getTimestamp()} Seluruh Tasks sudah dikerjakan`)
+      );
+    }
+  } catch (error) {
+    console.log(
+      chalk.red(`${getTimestamp()} Error saat memproses tasks:`, error.message)
+    );
+  }
+};
+
+const updateConvertedTokens = (token, farmingInfo) => {
+  const existingToken = convertedTokens.find((t) => t.token === token);
+  if (!existingToken) {
+    convertedTokens.push({ token, ...farmingInfo });
+    fs.writeFileSync(
+      convertedTokensPath,
+      JSON.stringify(convertedTokens, null, 2)
+    );
+  } else {
+    existingToken.activeFarmingStartedAt = farmingInfo.activeFarmingStartedAt;
+    existingToken.farmingDurationInSec = farmingInfo.farmingDurationInSec;
+    fs.writeFileSync(
+      convertedTokensPath,
+      JSON.stringify(convertedTokens, null, 2)
+    );
+  }
+};
+
+const runAccount = async (token, index) => {
+  console.log(`-----------------------------`);
+  console.log(
+    chalk.blue(`${getTimestamp()} Memproses akun ke-${index + 1}...`)
+  );
+  const farmingInfo = await getFarmingInfo(token);
+
+  if (farmingInfo) {
+    updateConvertedTokens(token, farmingInfo);
+
+    await processTasks(token, index);
+
+    const { activeFarmingStartedAt, farmingDurationInSec } = farmingInfo;
+    const nextClaimTime = DateTime.fromISO(activeFarmingStartedAt)
+      .plus({ seconds: farmingDurationInSec + 10 })
+      .setZone("Asia/Jakarta");
+    const waitTime = nextClaimTime.diffNow().as("milliseconds");
+    const hours = Math.floor(waitTime / 1000 / 60 / 60);
+    const minutes = Math.floor((waitTime / 1000 / 60) % 60);
+    console.log(
+      chalk.bgBlackBright(
+        `${getTimestamp()} Akun ke-${
+          index + 1
+        } menunggu ${hours} jam dan ${minutes} menit sebelum klaim...`
+      )
+    );
+    console.log(`-----------------------------`);
+    setTimeout(() => claimReward(token, index), Math.max(0, waitTime));
+  }
+};
+
+const startClaiming = async () => {
+  while (true) {
+    for (let i = 0; i < tokens.length; i++) {
+      runAccount(tokens[i], i);
+      await new Promise((resolve) => setTimeout(resolve, 5000)); // Delay 5 seconds between each account
+    }
+    console.log(
+      chalk.blue(`${getTimestamp()} Menunggu 1 jam sebelum memulai kembali...`)
+    );
+    await new Promise((resolve) => setTimeout(resolve, 60 * 60 * 1000)); // Wait 1 hour before starting again
+  }
+};
+
+startClaiming();
